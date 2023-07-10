24688975e0d7fb77f4271bc82982ae387a59c769
Dee <99658722+DeeJ4yNg@users.noreply.github.com>
2023-07-10 17:33:07 +0800

    Add files via upload

---
+++ Model_nanollama.py
@@ -0,0 +1,297 @@
+######################################################################################################################################################
+#                                               Name: Model_GPT                                                                                      #
+#                                               Comments Author: Devin Wu                                                                            #
+#                                               Date: Jul, 6, 2023 11:05AM                                                                           #
+#                                               Ref: http://pointborn.com/article/2022/2/18/1820.html                                                #
+#                                                    https://blog.csdn.net/Mikeyboi/article/details/119522689                                        #
+#                                                                                                                                                    #
+######################################################################################################################################################
+
+import math
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+def gelu(x):
+    return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+
+def relu(x):
+    return x if x > 0 else 0
+
+class LayerNorm(nn.Module):
+    def __init__(self, ndim, bias):
+        super().__init__()
+        # 初始化权重，标准化公式x_hat = x-u/根号下方差a^2-epsilon加入可学习参数，变成 output = alpha * x_hat + beta, 其中alpha相当于权重，beta相当于偏置。
+        self.weight = nn.Parameter(torch.ones(ndim))
+        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+
+    def forward(self, input):
+        # F.layer_norm(x, normalized_shape, self.weight.expand(normalized_shape)--自定义的weight, self.bias.expand(normalized_shape))--自定义的bias
+        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+
+# Root Mean Squra Normalization，Llama开源代码实现
+class RMSNorm(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+
+    def _norm(self, x):
+        # a = x * 1 / 根号下x^2.Mean + eps
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x):
+        output = self._norm(x.float()).type_as(x)
+        return output * self.weight # a_hat = wa, 无bias
+
+# 苏神的RoPE旋转位置编码
+class RotaryPositionalEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.base = config.base
+        self.d = config.n_embd // config.n_head
+        self.cos_cached = None
+        self.sin_cached = None
+
+    # 计算cosmtheta和sinmtheta并存起来，方便后续做乘法
+    def _build_cache(self, x: torch.Tensor):
+        if self.cos_cached is not None and x.shape[2] <= self.cos_cached.shape[2]:
+           return
+        seq_len = x.shape[2]
+        # 计算theta, theta = θi​ = 10000−d2(i−1)​,i∈[1,2,...,2d​]
+        theta = 1. / (self.base ** (torch.arange(0, self.d, 2).float() / self.d)).to(x.device)
+        # 序列长度绝对位置信息
+        seq_idx = torch.arange(seq_len, device=x.device).float().to(x.device)
+        # seq_idx和theta做外积
+        idx_theta = torch.einsum('n,d->nd', seq_idx, theta)
+        # 把外积的结果矩阵扩充至两倍
+        idx_theta2 = torch.cat([idx_theta, idx_theta], dim=1)
+        # 改变三角函数矩阵维度，适应输入维度，注意维度变化，要None掉B和nh
+        self.cos_cached = idx_theta2.cos()[None, None, :, :]
+        self.sin_cached = idx_theta2.sin()[None, None, :, :]
+
+    # 构造输入一半添加负号
+    def _neg_half(self, x: torch.Tensor):
+        d_2 = self.d // 2
+        return torch.cat([-x[:, :, :, d_2:], x[:, :, :, :d_2]], dim=-1)
+    
+    def forward(self, x: torch.Tensor): # x (B, nh, T, hs)
+        # 输入张量x流入_build_cache，得到cos_cached和sin_cached
+        self._build_cache(x)
+        # 可控制需要RoPE的维度
+        x_rope, x_pass = x[..., :self.d], x[..., self.d:]
+        # 构造’半负矩阵‘
+        neg_half_x = self._neg_half(x_rope)
+        # 根据苏神的公式逐项相乘
+        x_rope = (x_rope * self.cos_cached[:x.shape[2]]) + (neg_half_x * self.sin_cached[:x.shape[2]])
+        # 最后把有apply RoPE的和没有apply的feature组合起来
+        return torch.cat((x_rope, x_pass), dim=-1)
+    
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        #确保词嵌入的总维度数可以整除head的数量，不然每个head的维度不一样
+        assert config.n_embd % config.n_head == 0
+        #输入维度投影到三倍自身的维度，后续分别为q,k,v操作
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        #定义输出的线性层
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        #定义dropout和其他属性
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        self.RoPE = RotaryPositionalEmbeddings(config)
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        # B，T，C 分别表示 batch_size, sequence_length, embedding_dimension
+        B, T, C = x.size() 
+        # 定义q,k,v
+        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        # 更改q,k,v的维度，变成(batch_size, n_head, sequence_length, head_size)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # apply RoPE
+        k = self.RoPE(k)
+        q = self.RoPE(q)
+
+        # 根据公式计算self attention，维度变化：(B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # qk^T/根号d(k的最后一维即head_size也就是论文中的d)
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf')) # 添加decoder的mask矩阵，右上三角为负无穷防止训练过程中后面的token信息泄露
+        att = F.softmax(att, dim=-1) # Softmax(qk^T/根号d)
+        att = self.attn_dropout(att)
+        y = att @ v # Softmax(qk^T/根号d)V, 维度变化：(B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # 输出y维度重组，y.transpose(1, 2) -> (B, T, nh, hs), .view(B, T, C)-> (B, T, nh X hs) = (B,T,C)
+
+        # 输出y过线性层再dropout，最后return出去
+        y = self.resid_dropout(self.c_proj(y))
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # 定义一个全连接层，从输入维度投影到4倍输入维度的隐藏层，再输出与输入相同的维度
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = F.silu(x) # swiglu
+        x = self.c_proj(x)
+        x = self.dropout(x)
+        return x
+
+class Block(nn.Module):
+    # 把上面定义的模块组合成一个block，以便于之后堆叠，这里就实现了论文里的GPT结构图
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = RMSNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = RMSNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x)) # Pre Norm
+        x = x + self.mlp(self.ln_2(x)) # 加上Residual
+        return x
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # Token size
+    vocab_size: int = 50304 # 词汇表大小
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True # LayerNorm 和线性层的偏置，为了快选择了false
+    base: int = 10000 # RoPE用来计算theta的
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        # 断言一定得有Vocab_size 和 Block_size
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.config = config
+
+        # 构造一个transformer的字典用于方便后续堆叠
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), # Word Embedding
+            #wpe = nn.Embedding(config.block_size, config.n_embd), # Position Embedding, 这里直接用nn.Embedding来构造可学习的位置嵌入，而没有使用更新潮的RoPE等POS
+            drop = nn.Dropout(config.dropout),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), # Block 根据超参layer数来堆叠
+            ln_f = LayerNorm(config.n_embd, bias=config.bias),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # TODO 这是什么鬼？
+        self.transformer.wte.weight = self.lm_head.weight # 与上面的lm_head参数共享
+
+        # 行起_init_weights来初始化参数
+        self.apply(self._init_weights)
+
+        # 根据GPT2论文，residual projections的权重参数应按如下初始化
+        for pn, p in self.named_parameters():
+            print(pn,p)
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+
+    def get_num_params(self):
+        # 获取模型所有参数量，位置嵌入参数会被减去
+        n_params = sum(p.numel() for p in self.parameters())
+        #if non_embedding:
+        #    n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        # 如果module是Linear Layer，就按均值为0标准差为0.02的高斯分布初始化权重
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            # 如果是Linear Layer有偏置，则初始化为0
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        # 如果module是Embedding Layer，就按均值为0标准差为0.02的高斯分布初始化权重
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    # 前向过程
+    def forward(self, idx, targets=None):
+        device = idx.device
+        b, t = idx.size() # b = batch size, t = block size(输入的长度)
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t) tensor[0,1,2,3...t]
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx) # word token embedding (b, t, n_embd)
+        #pos_emb = self.transformer.wpe(pos) # position embedding (1, t, n_embd) TODO 考虑换成RoPE吧。。那不就变成LLAMA了么呵、
+        x = self.transformer.drop(tok_emb) # 词嵌入和位置嵌入相加，再过dropout
+        # 输入x流经block 堆叠
+        for block in self.transformer.h:
+            x = block(x)
+        
+        # 从堆叠出来后再过LayerNorm
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            # 最后x经过lm输出头(一个Linear Layer)输出logits。如果有加训练目标(ground truth)，就算上loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        else:
+            # 如果是推理阶段，则取最后一字的分数
+            logits = self.lm_head(x[:, [-1], :])
+            loss = None
+
+        return logits, loss
+
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        # 遍历模型参数
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # 过滤冻结的参数
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # 对所有2维及以上的参数作衰减
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        # 构建衰减条件
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # 构建AdamW优化器
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas)
+
+        return optimizer
+    
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+        for _ in range(max_new_tokens):
+            # 把过长的输入idx剪成block size
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            # 剪过的idx流入model，得出分数
+            logits, _ = self(idx_cond)
+            # 取最后一个分数，并根据温度参数缩放
+            logits = logits[:, -1, :] / temperature
+            # 若有topk，则从topk中采样
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            probs = F.softmax(logits, dim=-1)
+            # 从分布中采样
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # 在序列后cat上采样的idx后继续循环
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
---
+++ Training.py
@@ -0,0 +1,232 @@
+######################################################################################################################################################
+#                                               Name: Training_GPT                                                                                   #
+#                                               Comment Author: Devin Wu                                                                             #
+#                                               Date: Jul, 5, 2023 10:25AM                                                                           #
+#                                               Ref: http://pointborn.com/article/2022/2/18/1820.html                                                #
+#                                                    https://blog.csdn.net/Mikeyboi/article/details/119522689                                        #
+#                                                                                                                                                    #
+######################################################################################################################################################
+
+
+import os
+import time
+import math
+import pickle
+from contextlib import nullcontext
+
+import numpy as np
+import torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+from Model_nanollama import GPTConfig, GPT
+
+# 训练参数
+out_dir = 'out'
+eval_interval = 20
+log_interval = 1
+eval_iters = 50
+eval_only = False
+always_save_checkpoint = True
+#init_from = 'scratch'
+
+# 常规参数
+dataset = 'Material' 
+gradient_accumulation_steps = 5 * 8
+batch_size = 6 # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 512
+# model参数
+n_layer = 4
+n_head = 4
+n_embd = 128
+dropout = 0.0
+bias = False
+# adamw参数
+learning_rate = 6e-4
+max_iters = 6000
+weight_decay = 1e-1
+beta1 = 0.9 #Adam 公式中的俩β
+beta2 = 0.95
+grad_clip = 1.0 # 梯度裁剪
+# learning rate decay
+decay_lr = True
+warmup_iters = 200
+lr_decay_iters = 2000
+min_lr = 6e-5
+
+# 系统参数
+device = 'cpu'
+dtype = 'bfloat16'
+compile = False # Pytorch 2.0 新功能，需Cuda
+
+torch.manual_seed(1337) #设定随机种子
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+device_type = 'cuda' if 'cuda' in device else 'cpu'
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+
+# Autocast, 只支持CUDA
+# autocast 应该只封装网络的前向传播 (forward pass(es))，以及损失计算 (loss computation(s))。
+# 反向传播不推荐在 autocast 区域内执行，反向传播的操作会自动以对应的前向传播的操作的数据类型运行。
+ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+
+data_dir = os.path.join('data', dataset)
+# 内存映射
+train_data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+val_data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,)) #随机取batch_size个数，范围是[0,len(data) - block_size]
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix]) #遍历上面ix的结果然后把窗口扩展到block_size大小作为input
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix]) #同上，遍历上面ix的结果然后把窗口扩展到block_size大小再shift right作为output/ground truth
+    if device_type == 'cuda':
+        # 如果你有cuda用，就把数据PIN到GPU
+        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+    else:
+        x, y = x.to(device), y.to(device)
+    return x, y
+
+iter_num = 0
+best_val_loss = 1e9
+
+# 看看下载的dataset里面有没有包含词汇表
+meta_path = os.path.join(data_dir, 'meta.pkl')
+meta_vocab_size = None
+if os.path.exists(meta_path):
+    with open(meta_path, 'rb') as f:
+        meta = pickle.load(f)
+    meta_vocab_size = meta['vocab_size']
+    print(f"found vocab_size = {meta_vocab_size} (inside {meta_path})")
+
+model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
+                  bias=bias, vocab_size=None, dropout=dropout) # 创建字典保存模型参数
+
+# 从0开始训练
+print("Initializing a new model from scratch")
+# 定义词汇表大小
+if meta_vocab_size is None:
+    print("defaulting to vocab_size of GPT-2 to 50304 (50257 rounded up for efficiency)")
+
+# 如果dataset没有自带，则定义为50304
+model_args['vocab_size'] = meta_vocab_size if meta_vocab_size is not None else 50304
+gptconf = GPTConfig(**model_args)
+model = GPT(gptconf)
+model.to(device)
+
+
+# 混合精度，使用梯度缩放加快训练速度，AMP = Automatic Mixed Precision，如果dtype设成float16才会启用。
+# 如果前向传播对于一个特定的操作的输入是 float16 数据类型的，那么该操作对应的反向传播也会产生 float16 的梯度。
+# 小幅值的梯度值可能在半精度下是不可表示的。这些值可能会刷新为零 (称为underflow)，因此对应参数的更新也会丢失。
+# (这里可能是指类似梯度消失的问题，半精度下，如果梯度幅值小，权重在更新时几乎没有改变。)
+# 为了避免 underflow，采用梯度缩放(gradient scaling)的方式，将网络的损失乘以一个缩放因子，并对缩放后的损失调用反向传播，
+# 然后，通过网络反向流动的梯度将按相同的因子进行缩放。也就是说，缩放后梯度值会有一个较大的幅值，因此它们不会被刷新为零。
+# 每个参数的梯度在优化器 (optimizer) 更新参数之前应该是未缩放的，所以缩放因子不会影响学习率。
+scaler = torch.cuda.amp.GradScaler(enabled=(dtype == 'float16'))
+
+# 优化器
+optimizer = model.configure_optimizers(weight_decay, learning_rate, (beta1, beta2), device_type)
+checkpoint = None
+
+if compile:
+    print("compiling the model... (takes a ~minute)")
+    unoptimized_model = model
+    model = torch.compile(model) # 新功能，加速训练，需pytorch 2.0+，Cuda
+
+# 记录损失
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            with ctx:
+                logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# Schedule learning rate
+def get_lr(it):
+    # 当iter数小于warmup步数时，执行线性warmup
+    if it < warmup_iters:
+        return learning_rate * it / warmup_iters
+    # 当执行步数大于学习率衰减步数时，使用最小学习率
+    if it > lr_decay_iters:
+        return min_lr
+    # 当步数在warmup步数和学习率衰减步数之间时，执行余弦退火构建新的学习率  2000 < it < 600000
+    decay_ratio = (it - warmup_iters) / (lr_decay_iters - warmup_iters)
+    assert 0 <= decay_ratio <= 1 # 衰减率应为0~1的小数
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (learning_rate - min_lr)
+
+# training loop
+X, Y = get_batch('train') # 用训练的方式获取数据
+t0 = time.time()
+local_iter_num = 0 # 初始化iter num
+while True:
+    # 训练开始前，查看是否有设置学习率衰减，若是执行上面的Schedule learning rate方法，否则使用默认学习率
+    lr = get_lr(iter_num) if decay_lr else learning_rate
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+    # 每20步计算一次训练集和验证集损失，并写入checkpoint（没有GPU,非常慢）
+    if iter_num % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        if losses['val'] < best_val_loss or always_save_checkpoint:
+            best_val_loss = losses['val']
+            if iter_num > 0:
+                checkpoint = {
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'model_args': model_args,
+                    'iter_num': iter_num,
+                    'best_val_loss': best_val_loss,
+                }
+                print(f"saving checkpoint to {out_dir}")
+                torch.save(checkpoint, os.path.join(out_dir, 'ckpt.pt'))
+    
+    # 查看是否只是evaluate模式，是则退出训练过程
+    if iter_num == 0 and eval_only:
+        break
+
+    # 梯度累加
+    # 如果显存不足，我们可以通过gradient_accumulation_steps梯度累计来解决。
+    # 假设原来的batch size=10,数据总量为1000，那么一共需要100train steps，同时一共进行100次梯度更新。
+    # 若是显存不够，我们需要减小batch size，我们设置gradient_accumulation_steps=2，那么我们新的batch size=10/2=5。
+    # 我们需要运行两次，才能在内存中放入10条数据，梯度更新的次数不变为100次，那么我们的train steps=200
+    for micro_step in range(gradient_accumulation_steps):
+        with ctx:
+            logits, loss = model(X, Y)
+            loss = loss / gradient_accumulation_steps # scale the loss to account for gradient accumulation
+        # immediately async prefetch next batch while model is doing the forward pass on the GPU
+        X, Y = get_batch('train')
+        # 如果是fp16半精度才会产生梯度缩放的功能
+        scaler.scale(loss).backward()
+
+    # 梯度裁剪
+    if grad_clip != 0.0: #max norm
+        scaler.unscale_(optimizer) # 使用unscale_()取消梯度的缩放，以使得裁剪没有缩放的梯度:
+        torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+    # 常规训练步骤
+    scaler.step(optimizer)
+    scaler.update()
+    optimizer.zero_grad(set_to_none=True)
+
+    # 时间更新
+    t1 = time.time()
+    dt = t1 - t0
+    t0 = t1
+    if iter_num % log_interval == 0:
+        lossf = loss.item() * gradient_accumulation_steps
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms")
+    iter_num += 1
+    local_iter_num += 1
+
+    # 达到最大iter num退出循环
+    if iter_num > max_iters:
+        break
